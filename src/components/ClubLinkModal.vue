538c04d91eb501a48b5cdec094c2bc85fb238922
Sergio Saez <sergiosaez94@gmail.com>
2022-06-19 00:21:04 +0200

    modal to link a club

---
+++ src/components/ClubLinkModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="modal-wrapper">
+    <div class="modal-component">
+      <div class="modal-header">
+        <h3>Select a club</h3>
+      </div>
+      <div class="modal-body">
+        <clubs-table-vue @selected="selectClub($event)"></clubs-table-vue>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import ClubsTableVue from "./ClubsTable.vue";
+
+const emits = defineEmits(["save"]);
+
+const selectClub = ($event) => {
+  emits("save", { clubId: $event.clubId });
+};
+</script>
+
+<style></style>
